8f9788119abd181f0bf4aa81ec7105a5c5eb6dc9
boyangx <boyangx@umich.edu>
2021-06-15 11:34:39 -0400

    complete locally

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 
 import { CardList } from "./components/card-list/card-list.component";
+import { SearchBox } from "./components/search-box/search-box.component";
+
 
 import "./App.css";
 
@@ -30,10 +32,14 @@ class App extends React.Component {
     })
     return (
       <div className="App">
-        <input
-          type="search"
-          placeholder="search monsters"
-          onChange={(e) => this.setState({ searchField: e.target.value })}
+        <h1>Monsters Rolodex</h1>
+        <SearchBox
+          placeholder='search monsters'
+          handleChange={
+            e => {
+              this.setState({ searchField: e.target.value })
+            }
+          }
         />
         <CardList monsters={fileredMonsters} />
       </div>
